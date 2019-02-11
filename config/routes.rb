386cffc0bc3fff2366ebe034ac8a3607bd3589e0
rykyou <rkim1214@gmail.com>
2019-02-11 12:49:13 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index]
-      resources :visits, only: [:index, :create]
+      resources :visits, only: [:index, :create, :destroy]
       resources :artworks, only: [:index]
     end
   end
